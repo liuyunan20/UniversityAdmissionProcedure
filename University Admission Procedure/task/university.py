1d777ba915644d7107c574b1c3d13567bcf63584
liuyunan <liuyunan.20@gmail.com>
2022-02-10 08:26:14 -0800

    calculate mean score

---
+++ University Admission Procedure/task/university.py
@@ -0,0 +1,6 @@
+score_1 = int(input())
+score_2 = int(input())
+score_3 = int(input())
+score_mean = (score_1 + score_2 + score_3) / 3
+print(score_mean)
+print("Congratulations, you are accepted!")
