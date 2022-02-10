117d4f6923fe00ea01d6179f980eb66a36dedcde
liuyunan <liuyunan.20@gmail.com>
2022-02-10 10:20:19 -0800

    check if mean score satisfies the request

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -3,4 +3,7 @@ score_2 = int(input())
 score_3 = int(input())
 score_mean = (score_1 + score_2 + score_3) / 3
 print(score_mean)
-print("Congratulations, you are accepted!")
+if score_mean >= 60:
+    print("Congratulations, you are accepted!")
+else:
+    print("We regret to inform you that we will not be able to offer you admission.")
