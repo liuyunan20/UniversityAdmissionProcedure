658027d8b50d83c42eeb178d9b20a9a7fc60e5a7
liuyunan <liuyunan.20@gmail.com>
2022-02-12 23:54:29 -0800

    sort list in the descending order

---
+++ Topics/Sorting a list/Sort the flowers/main.py
@@ -0,0 +1,4 @@
+# the following line creates a list from the input, do not modify it, please
+prices = [float(price) for price in input().split()]
+print(sorted(prices, reverse=True))
+# your code below
